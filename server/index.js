41d0659e93c5c19b5a26382d7be8bb119d6f80e1
Faraz <104452510+GeekkyCoder@users.noreply.github.com>
2023-08-11 12:10:22 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ app.use(mongooseSanitize());
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cors({
-  origin:"https://reader-blogs.onrender.com",
+  origin:"https://reader-mern-web-app-gi6y.vercel.app",
   credentials:true
 }));
 app.use(fileUpload({ useTempFiles: true }));
